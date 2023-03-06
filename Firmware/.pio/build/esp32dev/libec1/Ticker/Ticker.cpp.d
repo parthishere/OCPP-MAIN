1179a225bc6c7509ffa7eecaf1be55c2357ddb8f
i1.griden <i1.griden@gmail.com>
2023-03-06 10:34:23 +0530

    included libraries for ACS712 and ZMPT101B

--- Firmware/.pio/build/esp32dev/libec1/Ticker/Ticker.cpp.d
+++
@@ -1,6 +0,0 @@
-.pio/build/esp32dev/libec1/Ticker/Ticker.cpp.o: \
- C:/Users/aagam/.platformio/packages/framework-arduinoespressif32/libraries/Ticker/src/Ticker.cpp \
- C:/Users/aagam/.platformio/packages/framework-arduinoespressif32/libraries/Ticker/src/Ticker.h \
- C:/Users/aagam/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_timer/include/esp_timer.h \
- C:/Users/aagam/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
- C:/Users/aagam/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h
